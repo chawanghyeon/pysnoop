cf74d6d006ab1772482be54083e83f9df805f397
chawanghyeon <chawanghyeon@gmail.com>
2025-05-22 00:24:17 +0900

    Feature: css 변경

--- widgets/system_info_widget.py
+++ widgets/system_info_widget.py
@@ -12,21 +12,6 @@ from textual.widgets import Static  # 개별 정보 표시에 사용
 class SystemInfoWidget(Widget):  # Static 대신 Widget을 상속받도록 변경
     """시스템 CPU 및 메모리 정보를 표시하는 스크롤 가능한 위젯"""
 
-    DEFAULT_CSS = """
-    SystemInfoWidget {
-        height: auto; /* 위젯의 높이를 자동으로 설정 */
-        max-height: 10; /* 위젯의 최대 높이 (10줄로 예시, 필요에 따라 조절) */
-        /* border: round green; */ /* 디버깅 또는 디자인용 테두리 */
-    }
-    VerticalScroll {
-        /* VerticalScroll 내부의 스크롤바 스타일링 (선택 사항) */
-        scrollbar-background: $primary-background-darken-2;
-        scrollbar-background-hover: $primary-background-darken-1;
-        scrollbar-color: $primary;
-        scrollbar-color-hover: $secondary;
-    }
-    """
-
     cpu_usage_overall: reactive[float] = reactive(0.0)
     cpu_usage_per_core: reactive[Dict[str, float]] = reactive({})
     mem_usage_percent: reactive[float] = reactive(0.0)
